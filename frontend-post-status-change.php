b79675e5d6c8c23f69c562d3ff4614724df6706f
resqonline <mail@resqonline.de>
2017-12-07 18:46:42 +0100

    updated function 07-12-2017

--- frontend-post-status-change.php
+++ frontend-post-status-change.php
@@ -1,41 +1,44 @@
 <?php
 //function to print custom status change buttons
 function show_pause_button(){
-    global $post;
     ?>
-    <form name="front_end_pause" method="POST" action="">
-        <input type="hidden" name="pid" id="pid" value="<?php echo $post->ID; ?>">
+    <form id="pause-<?php echo $post_id; ?>" name="front_end_pause" method="POST" action="">
+        <input type="hidden" name="pid" id="pid" value="<?php echo $post_id; ?>">
         <input type="hidden" name="FE_PAUSE" id="FE_PAUSE" value="FE_PAUSE">
-        <input type="submit" name="submit" id="submit" value="Deaktivieren">
+        <input type="submit" name="submit" id="submit" value="Kampagne deaktivieren">
     </form>
     <?
-	if (isset($_POST['FE_PAUSE']) && $_POST['FE_PAUSE'] == 'FE_PAUSE'){
-	    if (isset($_POST['pid']) && !empty($_POST['pid'])){
-	        change_post_status((int)$_POST['pid'],'paused');
-	    }
-	}
+    if (isset($_POST['FE_PAUSE']) && $_POST['FE_PAUSE'] == 'FE_PAUSE'){
+        $pid = $_POST['pid'] != '' ? $_POST['pid'] : '';
+        $post_stuff = array(
+            'ID' => $pid,
+            'post_status' => 'paused',
+        );
+
+        if ( $pid != ''){
+            wp_update_post( $post_stuff );
+        }
+    }
 }
 
 function show_resume_button(){
-    global $post;
     ?>
-    <form name="front_end_resume" method="POST" action="">
-        <input type="hidden" name="pid" id="pid" value="<?php echo $post->ID; ?>">
+    <form id="resume-<?php echo $post_id; ?>" name="front_end_resume" method="POST" action="">
+        <input type="hidden" name="pid" id="pid" value="<?php echo $post_id; ?>">
         <input type="hidden" name="FE_RESUME" id="FE_RESUME" value="FE_RESUME">
-        <input type="submit" name="submit" id="submit" value="Aktivieren">
+        <input type="submit" name="submit" id="submit" value="Kampagne aktivieren">
     </form>
     <?
-	if (isset($_POST['FE_RESUME']) && $_POST['FE_RESUME'] == 'FE_RESUME'){
-	    if (isset($_POST['pid']) && !empty($_POST['pid'])){
-	        change_post_status((int)$_POST['pid'],'pending');
-	    }
-	}
-}
+    if (isset($_POST['FE_RESUME']) && $_POST['FE_RESUME'] == 'FE_RESUME'){
+        $pid = $_POST['pid'] != '' ? $_POST['pid'] : '';
+        $post_stuff = array(
+            'ID' => $pid,
+            'post_status' => 'pending',
+        );
 
-//function to update post status
-function change_post_status( $post_id, $status ){
-    $current_post = get_post( $post_id, 'ARRAY_A' );
-    $current_post['post_status'] = $status;
-    wp_update_post( $current_post );
+        if ( $pid != ''){
+            wp_update_post( $post_stuff );
+        }
+    }
 }
 ?>
