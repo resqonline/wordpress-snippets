dae81b618de768f024c7b44455a19f28a9243aad
resqonline <mail@resqonline.de>
2018-08-02 21:58:57 +0200

    changes 02-08-2018
    corrected toggle

--- faq-posts/faq-scripts.js
+++ faq-posts/faq-scripts.js
@@ -5,14 +5,7 @@
     $('.faq-title').click(function () {
       var my_content_id = $(this).attr('data-content-id');
       $('#' + my_content_id).slideToggle();
-      $(this).toggleClass(function () {
-        if ($(this).is('.close-faq')) {
-          return 'open-faq';
-        } else {
-          return 'close-faq';
-        }
-
-      });
+      $(this).toggleClass('close-faq');
     });
   });
 })(jQuery);
