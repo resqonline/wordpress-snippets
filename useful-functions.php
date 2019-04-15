7ded94291ae75c2c28750c196c88193bd4a6b885
resqonline <mail@resqonline.de>
2019-04-15 19:48:18 +0200

    Update useful-functions.php

--- useful-functions.php
+++ useful-functions.php
@@ -17,4 +17,27 @@ function blockusers_init() {
     }
 }
 
+
+/*
+ * Add custom logo to WP login screen
+ */
+function logo_filter_login_head() {
+ 
+    if ( has_custom_logo() ) :
+ 
+        $image = wp_get_attachment_image_src( get_theme_mod( 'custom_logo' ), 'full' );
+        ?>
+        <style type="text/css">
+            .login h1 a {
+                background-image: url(<?php echo esc_url( $image[0] ); ?>);
+                background-size: contain;
+                height: 120px;
+                width: 320px;
+            }
+        </style>
+        <?php
+    endif;
+}
+add_action( 'login_head', 'logo_filter_login_head', 100 );
+
 ?>
