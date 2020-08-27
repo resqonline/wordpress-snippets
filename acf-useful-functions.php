4577130f4c9139e524ae43f486fd2da68bbdd071
resqonline <mail@resqonline.de>
2020-08-27 17:52:56 +0200

    Update acf-useful-functions.php
    added function for download file custom upload folder and redirect to disable hotlinking

--- acf-useful-functions.php
+++ acf-useful-functions.php
@@ -24,4 +24,36 @@ function rq_post_content_acf_name( $field ) {
 }
 add_filter('acf/load_field/name=_post_content', 'rq_post_content_acf_name');
 
+
+// Add custom directory for "Download" post type file upload field
+/*
+	Also add this to your htaccess file, to protect files uploaded in this directory to be accessed directly and be redirected to the download_entry single post based on the folder ('download_entry' is the custom post type name), exchange 'mydomain.com' with your domain:
+
+	# BEGIN Download redirect
+	<IfModule mod_rewrite.c>
+	RewriteEngine On
+	RewriteCond %{HTTP_REFERER} !^http(s)?://(www\.)?mydomain\.com/.*$ [NC]
+	RewriteCond %{REQUEST_URI} ^(.*?/?)wp-content/uploads/downloads/.* [NC]
+	RewriteRule ^wp-content/uploads/downloads/(.+)$ index.php?post_type=download_entry&p=$1 [L,QSA]
+	</IfModule>
+	#END Download redirect
+*/
+// Custom upload folder for Download files - exchange field name 'download_file' with your own field name
+add_filter('acf/upload_prefilter/name=download_file', 'download_file_upload_prefilter');
+function download_file_upload_prefilter( $errors ){
+	// in this filter we add a WP filter that alters the upload path
+	add_filter('upload_dir', 'download_file_upload_dir');
+	return $errors;
+}
+// we are adding the folder structure like /uploads/downloads/postID - this only works in wp admin!
+function download_file_upload_dir( $uploads ){
+	$post_id = ( isset( $_REQUEST['post_id'] ) ? $_REQUEST['post_id'] : '' );
+	$subdir = $post_id;
+	$uploads['subdir'] = $subdir;
+	$uploads['path'] = $uploads['basedir'].'/downloads/'.$subdir;
+	$uploads['url'] = $uploads['baseurl'].'/downloads/'.$subdir;
+	return $uploads;
+}
+
+
 ?>
